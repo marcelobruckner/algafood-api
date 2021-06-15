2baee3b7088cbbe7a1937e77dccb56500681d355
Luis Marcelo Bruckner <marcelobruckner@gmail.com>
2021-06-15 20:35:16 -0300

    ajustes no projeto

--- src/main/java/com/algaworks/algafood/domain/repository/PermissaoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/PermissaoRepository.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.domain.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algafood.domain.model.Permissao;
