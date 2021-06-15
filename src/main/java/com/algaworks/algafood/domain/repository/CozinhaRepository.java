35ba3fc95129d6f0919df92cb6cb07bd077cce3f
Luis Marcelo Bruckner <marcelobruckner@gmail.com>
2021-06-15 19:21:35 -0300

    versionando arquivos

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,10 +1,12 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
 
-//	List<Cozinha> consultarPorNome(String nome);
+	List<Cozinha> findByNome(String nome);
 }
