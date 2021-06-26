0cf35c15a03eba028533c93611d2aa67d18b5069
Marcelo Bruckner <marcelobruckner@gmail.com>
2021-06-26 08:38:12 -0300

    refatorando os repositorios do projeto

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,4 +18,3 @@ insert into cidade (id, nome, estado_id) values (3, "Rio de Janeiro", 2);
 
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
-    
