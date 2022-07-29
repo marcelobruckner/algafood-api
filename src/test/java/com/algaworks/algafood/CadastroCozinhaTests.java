429b1ea24387773e5ae9c82a7996e5b278c8b10d
Marcelo Bruckner <marcelobruckner@gmail.com>
2022-07-29 16:06:50 -0300

    Ajustando as migrações e testes

--- src/test/java/com/algaworks/algafood/CadastroCozinhaTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaTests.java
@@ -2,13 +2,16 @@ package com.algaworks.algafood;
 
 import static io.restassured.RestAssured.given;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
@@ -16,16 +19,22 @@ import io.restassured.http.ContentType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaTests {
 
   @LocalServerPort
   private int port;
+
+  @Autowired
+  private Flyway flyway;
 
   @Before
   public void setUp() {
     RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     RestAssured.port = port;
     RestAssured.basePath = "/cozinhas";
+
+    flyway.migrate();
   }
 
   @Test
@@ -40,13 +49,13 @@ public class CadastroCozinhaTests {
   }
 
   @Test
-  public void deveConter6Cozinhas_QuandoConsultarCozinhas() {
+  public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
     given()
         .accept(ContentType.JSON)
         .when()
         .get()
         .then()
-        .body("", Matchers.hasSize(6));
+        .body("", Matchers.hasSize(4));
     // .body("nome", Matchers.hasItems("Tailandesa", "Indiana", "Argentina",
     // "Brasileira"));
   }
