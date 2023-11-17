99ef6f0ba877d3e616fc3325caa1862f430c393b
Marcelo Bruckner <marcelobruckner@gmail.com>
2023-11-17 16:53:31 -0300

    refactor: Retirando Mixin que não será mais usado.

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -1,7 +1,6 @@
 package com.algaworks.algafood.core.jackson;
 
 import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
-import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -13,7 +12,6 @@ public class JacksonMixinModule extends SimpleModule {
     private static final long serialVersionUID = 1L;
 
     public JacksonMixinModule() {
-        setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
         setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
     }
 }
